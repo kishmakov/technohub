6851f66ffb0e64fafbf305e2b5875b6dd70c6b4c
Kirill Shmakov <menato@yandex-team.ru>
2013-04-14 21:51:21 +0400

    Subselection works.

--- vle/views.py
+++ vle/views.py
@@ -2,15 +2,38 @@ from django.template.loader import get_template
 from django.template import RequestContext
 from django.http import HttpResponse
 from django.utils import simplejson
+from django.db.models import Q
 
 from common.views import common_dict
 
 from models import SimpleFluid
 
-from common.settings import STATIC_URL, SITE_URL
+
+def parse_request(request):
+    dict = {}
+    if request.method == 'GET':
+        dict['query'] = request.GET.get('query', '')
+        if dict['query'] == 'string':
+            dict['string'] = request.GET.get('string', '')
+        if dict['query'] == 'number':
+            dict['number'] = request.GET.get('number', '')
+
+    return dict
 
 def data_base(request):
-    fluids = SimpleFluid.objects.all()
+    dict = parse_request(request)
+    fluids = []
+    if dict['query'] == 'string':
+        string = dict['string']
+        qname = Q(name__icontains=string)
+        qnum = Q(cas_num__icontains=string)
+        fluids = SimpleFluid.objects.filter(qname|qnum)[0:9]
+    elif dict['query'] == 'number':
+        number = dict['number']
+        fluids = SimpleFluid.objects.get(number=number)[0]
+    else:
+        fluids = SimpleFluid.objects.all()[0:9]
+
     lines = []
 
     for fluid in fluids:
