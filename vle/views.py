e2d94533edb37081ffa830094dcf1fed4ad053f9
Kirill Shmakov <menato@yandex-team.ru>
2013-04-16 23:06:37 +0400

    Begin of the JS-code organisation in pr.html movement.

--- vle/views.py
+++ vle/views.py
@@ -23,25 +23,34 @@ def parse_request(request):
 def data_base(request):
     dict = parse_request(request)
     fluids = []
+    detailed = False
     if dict['query'] == 'string':
         string = dict['string']
         qname = Q(name__icontains=string)
         qnum = Q(cas_num__icontains=string)
         fluids = SimpleFluid.objects.filter(qname|qnum)[0:9]
     elif dict['query'] == 'number':
+        detailed = True
         number = dict['number']
-        fluids = SimpleFluid.objects.get(number=number)[0]
+        fluid = SimpleFluid.objects.get(cas_num=number)
+        fluids.append(fluid)
     else:
         fluids = SimpleFluid.objects.all()[0:9]
 
     lines = []
 
     for fluid in fluids:
-        lines.append({
+        description = {
             'name' : fluid.name,
             'number' : fluid.cas_num,
             'formula' : fluid.formula
-        })
+        }
+
+        if detailed:
+            description['weight'] = fluid.mol_weight
+            description['tfp'] = fluid.tfp
+
+        lines.append(description)
 
     data = simplejson.dumps(lines)
 
